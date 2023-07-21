3db798e7d5a0f9de76ead89d113de310d8f04e03
Melkijo <jonathanandaraa@gmail.com>
2023-07-21 17:26:27 +0800

    chore: registration page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,21 @@ module.exports = {
             colors: {
                 "primary": "#007CBD",
                 "second": "#3EB7DD",
-                "dark": "#1B1E28"
+                "dark": "#1B1E28",
+                "input": "#F8F8F8"
             },
+            keyframes: {
+                slide: {
+                    '0%': { transform: 'translateX(0)' },
+                    ' 100%': { transform: 'translateX(-100%)' }
+                },
+                wiggle: {
+                    '0%, 100%': { transform: 'rotate(-3deg)' },
+                    '50%': { transform: 'rotate(3deg)' },
+                }
+
+            },
+
         },
     },
     plugins: [],
